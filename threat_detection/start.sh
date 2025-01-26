4dc601567ad2f5c95f2741fe21e4f5223dabe897
abdissa-png <abdissadegefu1@gmail.com>
2025-01-26 04:28:58 +0300

    Changes to DockerFile

--- threat_detection/start.sh
+++ threat_detection/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-daphne -b 0.0.0.0 -p $PORT threat_detection.asgi:application
+daphne -b 0.0.0.0 -p 8000 threat_detection.asgi:application
