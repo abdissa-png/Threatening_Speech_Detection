ea47d308b511920e2a6fcf85279b1b230592eb71
abdissa-png <abdissadegefu1@gmail.com>
2025-01-26 16:25:24 +0300

    fixes to wss

--- threat_detection/threat_detection/settings.py
+++ threat_detection/threat_detection/settings.py
@@ -76,10 +76,7 @@ ASGI_APPLICATION = 'threat_detection.routing.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
 
