2402155b126c870c4c1ea2f57af417331a1eedcc
abdissa-png <abdissadegefu1@gmail.com>
2025-01-26 12:23:47 +0300

    Fixing wss issues

--- threat_detection/threat_detection/routing.py
+++ threat_detection/threat_detection/routing.py
@@ -1,3 +1,4 @@
+# filepath: /home/abdissad/aait AI 4th +/5yr_1sem/NLP/Threatening_Speech_Detection/threat_detection/threat_detection/routing.py
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from django.core.asgi import get_asgi_application
@@ -5,7 +6,5 @@ from detector.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter([
-        path('ws/', URLRouter(websocket_urlpatterns))
-    ]),
+    "websocket": URLRouter(websocket_urlpatterns),
 })
